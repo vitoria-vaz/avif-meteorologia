832ee3d7f07c983ec5bcd06af79aa1fc208e73f4
felip <felipecrackaddict@gmail.com>
2025-04-03 04:41:54 -0300

    2.1. Mudar Paineis de gradientes para branco transparente Remover imports desnecessários

--- app/src/main/java/com/avif/meteorologia/ui/screen/components/AirQuality.kt
+++ app/src/main/java/com/avif/meteorologia/ui/screen/components/AirQuality.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -21,7 +20,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -29,11 +27,6 @@ import androidx.compose.ui.unit.dp
 import com.avif.meteorologia.R
 import com.avif.meteorologia.data.model.WeatherInfo
 import com.avif.meteorologia.ui.screen.util.fromHex
-import com.avif.meteorologia.ui.theme.CardGradient1
-import com.avif.meteorologia.ui.theme.CardGradient2
-import com.avif.meteorologia.ui.theme.CardGradient3
-import com.avif.meteorologia.ui.theme.ColorText
-import com.avif.meteorologia.ui.theme.ColorTextSecondary
 
 @Composable
 fun AirQuality(
--- app/src/main/java/com/avif/meteorologia/ui/screen/components/WeeklyForecast.kt
+++ app/src/main/java/com/avif/meteorologia/ui/screen/components/WeeklyForecast.kt
@@ -5,16 +5,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -23,21 +20,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.avif.meteorologia.ui.screen.util.ForecastData
 import com.avif.meteorologia.ui.screen.util.ForecastItem
-import com.avif.meteorologia.ui.screen.util.fromHex
-import com.avif.meteorologia.ui.theme.CardGradient1
-import com.avif.meteorologia.ui.theme.CardGradient2
-import com.avif.meteorologia.ui.theme.CardGradient3
-import com.avif.meteorologia.ui.theme.ColorText
-import com.avif.meteorologia.ui.theme.ColorTextSecondary
+
 
 @Composable
 fun WeeklyForecast(
